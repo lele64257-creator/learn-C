d4a29f94d08133cb2de42c4223728e6a7a999e51
lele <lele64257@gmail.com>
2026-07-13 11:48:15 +0800

    first C program

---
+++ first C.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) //main function
+{
+    int number;  /*这是一个多行注释
+                int 是一个声明
+                num 是一个变量
+                */
+    number=1;  //这也是注释 给num赋值为1
+
+    printf("I'm a novice.\n");//print function \+n是换行
+    printf("This is my first C programme.");
+
+    return 0;
+}
