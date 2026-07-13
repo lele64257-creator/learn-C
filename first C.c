4251724ad5487540317cb64840e8e2cb03b1bd42
lele <lele64257@gmail.com>
2026-07-13 11:55:03 +0800

    2save

--- first C.c
+++ first C.c
@@ -8,7 +8,8 @@ int main(void) //main function
     number=1;  //这也是注释 给num赋值为1
 
     printf("I'm a novice.\n");//print function \+n是换行
-    printf("This is my first C programme.");
-
+    printf("This is my first C programme.\n");
+    printf("this is a number %d.",number); //%+d打印变量num的值
+    
     return 0;
 }
