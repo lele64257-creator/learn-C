4ed543abd47b930370801280a0f981194214ec5e
lele <lele64257@gmail.com>
2026-07-14 09:34:33 +0800

    change fathm_ft.c

--- fathm_ft.c
+++ fathm_ft.c
@@ -1,4 +1,4 @@
-//fathm_ft.c 把2音寻转换为英寸
+//fathm_ft.c 把2音寻fathom转换为英寸feet
 #include<stdio.h>
 int main(void)
 {
@@ -8,8 +8,8 @@ int main(void)
     fathoms = 2;
     feet = fathoms * 6;
 
-    printf("There is %d feet in %d fathoms!\n",feet,fathoms);
-    printf("Yes, I said %d feet!\n",6 * fathoms);
+    printf("There is %d feet in %d fathoms!\n",feet,fathoms);//进行了两次替换：两个%d分别对应feet和fathoms中间用逗号隔开
+    printf("Yes, I said %d feet!\n",6 * fathoms);//将6 * fathoms的结果输出到%d中
 
     return 0;
 }
