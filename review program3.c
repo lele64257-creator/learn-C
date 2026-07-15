c531610a9c4d215adc3cdc0876b5e0924924853e
lele <lele64257@gmail.com>
2026-07-15 11:11:48 +0800

    7.15

---
+++ review program3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+
+    int age = 19;
+    int day = age * 365;
+
+    printf("I'm %d years old. I have lived for %d days.\n", age , day);
+
+    return 0;
+    
+}
