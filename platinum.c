2957ec70eb66285a5ce677794bbbaa4f1ab28065
lele <lele64257@gmail.com>
2026-07-17 10:26:21 +0800

    platinum.c

---
+++ platinum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>//platinum 白金
+int main(void)
+{
+    float weight;//float 小数
+    float value;
+
+    printf("Are you eorth your weight in platinum?\n");
+    printf("Let's chrck it out.\n");
+    printf("Please enter your weight in pounds:");
+
+    scanf("%f", &weight);//&地址符 取地址 阻塞等待输入
+    
+    value = 1700.0 *weight * 14.533;
+
+    printf("You weight in platinum is worth $%.2f.\n", value);//%+.+数字+f 保留小数点后几位
+    printf("You are easily worth that! If platinum prices drop,\n");
+    printf("Eat more to maintain your value.\n");
+
+    return 0;
+}
