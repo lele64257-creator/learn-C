c2a51eac7ca6dfd63ea3c669aeb158c901df4f7f
lele <lele64257@gmail.com>
2026-07-19 22:47:59 -0400

    other int.c

---
+++ other int.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main (void)
+{
+    short int num = 100;
+    //使用短整型，占用内存比int小，表示范围比int小
+    short int num01 = 10;
+    //可以简写short
+    long int num2 = 10000;
+    //使用长整型，占用内存比int大，表示范围比int大
+    //可以简写long
+    long long int num3 = 1;
+    //超长整形
+    printf("%d\n",num);
+    printf("%d\n",num01);
+    printf("%d\n",num2);
+
+    return 0;
+}
