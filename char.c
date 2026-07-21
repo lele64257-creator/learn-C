fbdd9b23d1f4017ebc9620652ddd2c80f5865157
lele <lele64257@gmail.com>
2026-07-20 21:48:22 -0400

    char.c

---
+++ char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+
+    char ch = 65;//8bit
+
+    char ch2 = 'A';
+
+    printf("int size = %d\n",sizeof(char));
+
+    printf("ch = %c\n",ch);
+
+    printf("ch2 = %d\n",ch2);
+    
+    return 0;
+}
