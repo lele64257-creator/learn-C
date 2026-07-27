b154374d11e581ca62ca86ab15536731b45a2677
Rooney <lele64257@gmail.com>
2026-07-27 16:17:27 -0700

    Update first C.c

--- first C.c
+++ first C.c
@@ -2,7 +2,7 @@
 int main(void) //main function
 {
     int number;  /*这是一个多行注释 int 是一个声明 num 是一个变量*/
-    number=1;  //这也是注释 给num赋值为1
+    number = 1;  //这也是注释 给num赋值为1
     float price = 2.5;//float单精度小数
     double pi = 3.1415;// 双精度小数
     char grade = 'A';//字符变量
