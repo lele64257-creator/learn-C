78102acbd3da3dc4c48029bd15712b037f4fa93d
Rooney <lele64257@gmail.com>
2026-07-27 17:16:30 -0700

    Update abc.c

--- abc.c
+++ abc.c
@@ -4,7 +4,7 @@ int main(void)
    int age;
    
    printf("How old are you\n");
-   scanf("%f",&age);
+   scanf("%d",&age);
    printf("You have been born in this world for %d seconds",age * 3.1536e7);
    
    return 0;
