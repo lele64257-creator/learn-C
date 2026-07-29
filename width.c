1260b5be4d44742c13c03c24e9b628e7b3a20c9c
Rooney <lele64257@gmail.com>
2026-07-29 17:23:22 -0400

    Add character width examples in width.c

---
+++ width.c
@@ -0,0 +1,12 @@
+//字符宽度
+#include<stdio.h>
+#define PAGES 959
+int main(void)
+{
+  printf("*%d*\n", PAGES);
+  printf("*%2d*\n", PAGES);
+  printf("*%10d*", PAGES);
+  printf("*%-10d*", PAGES);
+
+  return 0;
+}
